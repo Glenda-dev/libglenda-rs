7682ceb3eba99d5bee4fcddf2e692cb6aebe6244
2018wzh <wzh20070720@outlook.com>
2026-01-04 13:44:59 +0800

    add backtrace

--- src/console.rs
+++ src/console.rs
@@ -3,6 +3,15 @@ use crate::ipc::utcb;
 use core::fmt;
 use spin::Mutex;
 
+pub const ANSI_RESET: &str = "\x1b[0m";
+pub const ANSI_RED: &str = "\x1b[31m";
+pub const ANSI_GREEN: &str = "\x1b[32m";
+pub const ANSI_YELLOW: &str = "\x1b[33m";
+pub const ANSI_BLUE: &str = "\x1b[34m";
+pub const ANSI_MAGENTA: &str = "\x1b[35m";
+pub const ANSI_CYAN: &str = "\x1b[36m";
+pub const ANSI_WHITE: &str = "\x1b[37m";
+
 pub struct Console {
     cap: Option<CapPtr>,
 }
