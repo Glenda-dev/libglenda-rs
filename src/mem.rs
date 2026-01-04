964f4576b2fdbe84709edc6cc4aeb0c27b59bda9
2018wzh <wzh20070720@outlook.com>
2026-01-04 18:55:44 +0800

    add consts

--- src/mem.rs
+++ src/mem.rs
@@ -5,8 +5,10 @@ pub const TRAMPOLINE_VA: usize = VA_MAX - PGSIZE; // Trampoline 映射地址
 pub const TRAPFRAME_VA: usize = TRAMPOLINE_VA - PGSIZE; // Trapframe 映射地址
 pub const UTCB_VA: usize = TRAPFRAME_VA - PGSIZE; // UTCB 映射地址 0x3FFFFFD000
 pub const STACK_VA: usize = UTCB_VA - PGSIZE; // 用户栈映射地址
-pub const STACK_SIZE: usize = 16 * PGSIZE; // 16KB
-pub const HEAP_SIZE: usize = 1024 * PGSIZE; // 1MB
+pub const STACK_PAGES: usize = 4; // 用户栈页面数 4 * 4KB = 16KB
+pub const STACK_SIZE: usize = STACK_PAGES * PGSIZE; // 16KB
+pub const HEAP_PAGES: usize = 256; // 用户堆页面数 256 * 4KB = 1MB
+pub const HEAP_SIZE: usize = HEAP_PAGES * PGSIZE; // 1MB
 pub const HEAP_VA: usize = 0x2000_0000; // 用户堆地址
 pub const RES_VA_BASE: usize = 0x4000_0000; // 启动时提供的资源
 
