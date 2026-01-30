b00fe89ead1cdcd7acddca8a1bc98b5e6f8d7e64
2018wzh <wzh20070720@outlook.com>
2026-01-30 23:00:24 +0800

    [misc] Fix compile

--- src/runtime/service/mod.rs
+++ src/runtime/service/mod.rs
@@ -1,12 +1,14 @@
 use crate::arch::runtime::panic_break;
-use crate::cap::{CapPtr, Endpoint};
+use crate::cap::{CapPtr, Endpoint, Frame};
 use crate::error::code;
 use crate::ipc::utcb;
 use crate::ipc::{MsgFlags, MsgTag};
 use crate::protocol::process;
 
 pub const MONITOR_SLOT: usize = 4;
+pub const PLATFORM_SLOT: usize = 6;
 pub const MONITOR_CAP: Endpoint = Endpoint::from(CapPtr::new(MONITOR_SLOT));
+pub const PLATFORM_CAP: Frame = Frame::from(CapPtr::new(PLATFORM_SLOT));
 
 pub fn exit(code: usize) -> ! {
     let tag = MsgTag::new(process::PROCESS_PROTO, process::EXIT, MsgFlags::NONE);
