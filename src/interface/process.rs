fcde65da355e32f809f70d51f05b51dc4690f1ee
2018wzh <wzh20070720@outlook.com>
2026-02-03 16:55:29 +0800

    [interface]  Add spawn() to ProcessService

--- src/interface/process.rs
+++ src/interface/process.rs
@@ -2,7 +2,7 @@ use crate::error::Error;
 
 /// ProcessService provides high-level process control.
 pub trait ProcessService {
-    fn spawn(&mut self, name: &str) -> Result<usize, Error>;
+    fn spawn(&mut self, name: &str, privileged: bool) -> Result<usize, Error>;
     fn fork(&mut self, pid: usize) -> Result<usize, Error>;
     fn exit(&mut self, pid: usize, code: usize) -> Result<(), Error>;
     fn load_image(&mut self, pid: usize, elf_data: &[u8]) -> Result<(usize, usize), Error>;
