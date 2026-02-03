8f3736c458c11361bd40bd779ae1f53423575537
2018wzh <wzh20070720@outlook.com>
2026-02-03 14:36:01 +0800

    [ipc] Add Display for MsgFlags

--- src/ipc/msg.rs
+++ src/ipc/msg.rs
@@ -1,4 +1,5 @@
 use bitflags::bitflags;
+use core::fmt::Display;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(transparent)]
@@ -31,9 +32,30 @@ impl MsgTag {
 }
 
 bitflags! {
+    #[derive(Clone,Copy)]
     pub struct MsgFlags: usize {
         const NONE = 0;
         const OK = 1 << 0;
         const ERROR = 1 << 1;
+    }
+}
+
+impl Display for MsgFlags {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let mut first = true;
+        let perms = [(MsgFlags::OK, "OK"), (MsgFlags::ERROR, "ERROR")];
+        for (bit, name) in perms.iter() {
+            if self.contains(*bit) {
+                if !first {
+                    write!(f, "|")?;
+                }
+                write!(f, "{}", name)?;
+                first = false;
+            }
+        }
+        if first {
+            write!(f, "NONE")?;
+        }
+        Ok(())
     }
 }
